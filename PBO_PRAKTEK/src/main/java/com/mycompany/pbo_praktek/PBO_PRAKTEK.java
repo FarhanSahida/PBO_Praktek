fc24a6c9ea4efe0a87a9fff1751dceaddaad61d4
FarhanSahida <farhansahida1101@gmail.com>
2023-07-07 15:32:10 +0700

    .

---
+++ PBO_PRAKTEK/src/main/java/com/mycompany/pbo_praktek/PBO_PRAKTEK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_praktek;
+
+/**
+ *
+ * @author Hansy
+ */
+public class PBO_PRAKTEK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
